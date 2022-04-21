b1f00ebee6a6025b9dc30dc1366bdfee6565eb73
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-04-22 00:50:25 +0600

    order api and checkout api implemented

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+const admin = require("firebase-admin");
+
+const stripe = require("stripe")("sk_test_51JvoX2HPRrHHjKcv6nK6C6YyzniFyJYtwUIrqIYHFANfYKegUD4oE45rMwhWn7r0DcM9Nf0MqvxRCmIkwnPRzsde00efp5vNUW");
+const uuid = require("uuid").v4;
 
 
 const app = express();
@@ -12,8 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 
+//initialize firebase
+const serviceAccount = require("./click-bangla-firebase-adminsdk.json");
 
-
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 
 const port = process.env.PORT || 5000;
@@ -22,16 +30,37 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u0u6n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+//token authorization function
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers?.authorization?.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+
+    next();
+}
+
 
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db('organicBdDb');
+        const database = client.db('clickBanglaDB');
         const productsCollection = database.collection('products');
         const featuredCollection = database.collection('featuredProducts');
         const reviewCollection = database.collection('reviews');
+        const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
 
 
         //GET API for all products
@@ -66,6 +95,63 @@ async function run() {
             res.json(result);
         });
 
+        //POST API for users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        //GET API for single user
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+
+        //PUT API for save user in db with google and github
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updatedDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.json(result);
+
+        });
+
+        //PUT API for admin
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const requester = req.decodedEmail;
+            console.log(user);
+            console.log(requester)
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === "admin") {
+                    const updatedDoc = {
+                        $set: {
+                            role: 'admin'
+                        }
+                    };
+                    const result = await usersCollection.updateOne(filter, updatedDoc);
+                    console.log(result);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have permission to make admin' });
+            }
+
+        });
+
         //POST API for add review
         app.post('/reviews', async (req, res) => {
             const review = req.body;
@@ -78,6 +164,92 @@ async function run() {
             const cursor = reviewCollection.find({});
             const reviews = await cursor.toArray();
             res.json(reviews);
+        });
+
+
+
+        //GET API for user order
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            let orders;
+            if (email) {
+                const query = { email: email };
+                const cursor = ordersCollection.find(query);
+                orders = await cursor.toArray();
+
+            }
+            else {
+                const cursor = ordersCollection.find({});
+                orders = await cursor.toArray();
+            }
+            res.json(orders);
+
+        });
+
+        //POST API for ordered products
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+
+        });
+
+        //DELETE API for user order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
+
+
+
+        //stripe gateway  checkout
+        app.post("/checkout", async (req, res) => {
+            console.log("Request:", req.body);
+
+            let error;
+            let status;
+            try {
+                const { product, token } = req.body;
+
+                const customer = await stripe.customers.create({
+                    email: token.email,
+                    source: token.id,
+                });
+
+                const idempotency_key = uuid();
+                const charge = await stripe.charges.create(
+                    {
+                        amount: subtotal * 100,
+                        currency: "BDT",
+                        customer: customer.id,
+                        receipt_email: token.email,
+                        description: `Purchased the ${product.name}`,
+                        shipping: {
+                            name: token.card.name,
+                            address: {
+                                line1: token.card.address_line1,
+                                line2: token.card.address_line2,
+                                city: token.card.address_city,
+                                country: token.card.address_country,
+                                postal_code: token.card.address_zip,
+                            },
+                        },
+                    },
+                    {
+                        idempotency_key,
+                    }
+                );
+                console.log("Charge:", { charge });
+                status = "success";
+            } catch (error) {
+                console.error("Error:", error);
+                status = "failure";
+            }
+
+            res.json({ error, status });
         });
 
 
